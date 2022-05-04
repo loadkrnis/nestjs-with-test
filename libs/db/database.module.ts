33343a88edbac528882e5dcd9ecfde882e36b9a2
Charming <loadkrnis@gmail.com>
2022-05-04 23:19:19 +0900

    typeorm 데이터베이스 연결 작업
    Took 19 minutes

---
+++ libs/db/database.module.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+export const DatabaseModule = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3307,
+  username: 'root',
+  password: 'root',
+  database: 'test',
+  synchronize: true,
+  autoLoadEntities: true,
+  entities: ['libs/entity/src/**/*.entity.ts'],
+});
